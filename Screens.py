7ef1b99a38a0fbf2f1177bbaa7cefc5dedd1d484
rhendre <59066685+rhendre@users.noreply.github.com>
2020-04-16 15:01:27 -0400

    new m=screens file

--- Screens.py
+++ Screens.py
@@ -1,6 +1,7 @@
 # Main Class for Backend Editor
 # Has Instances of PollingScreen, Data Collection Screen, ShowCase Screen and Text Screen
 # Ruchi_Hendre@2020
+import os
 import sys
 
 from PyQt5 import QtWidgets
@@ -11,6 +12,7 @@ from Editor.PollingScreen import PollingScreen
 from Editor.Screen1 import Screen1
 from Editor.Screen2 import Screen2
 from Editor.ShowCaseScreen import ShowCaseScreen, UploadScreen
+from Editor.ConversationScreen import ConversationScreen
 
 
 def changeWindow(w1, w2):
@@ -21,15 +23,19 @@ def changeWindow(w1, w2):
 def changeDataCollectionScreen(w1, w2, template):
     DataCollectionScreen.template = template
     DataCollectionScreen.count = 0
+    w2.canvas.axes.clear()
     w2.readFromJsonFile()
     w1.hide()
     w2.show()
 
 
 def showWindow(w1):
-    w1.loadImage()
-    if ShowCaseScreen.VideoFileName != '':
-     w1.loadVideo()
+    print(ShowCaseScreen.doubleClicked)
+    if ShowCaseScreen.doubleClicked == 1:
+        w1.loadImage()
+    if ShowCaseScreen.VideoFileName != '' and ShowCaseScreen.doubleClicked == 1:
+        w1.loadVideo()
+    ShowCaseScreen.doubleClicked = 0
     w1.show()
 
 
@@ -48,6 +54,8 @@ def labelText(self, MainWindow, value):
 
 
 def main():
+    path = os.path.dirname(sys.argv[0])
+    print(os.path.dirname(sys.argv[0]))
     app = QtWidgets.QApplication(sys.argv)
     app.setStyle('Fusion')
     app.setStyleSheet("QTextBrowser { background-color: white; border-radius: "
@@ -62,17 +70,24 @@ def main():
     dataCollectionScreen = DataCollectionScreen()
     showCaseScreen = ShowCaseScreen()
     uploadScreen = UploadScreen()
+    conversationScreen = ConversationScreen()
 
     screen1.pushButton.clicked.connect(lambda: changeWindow(screen1, screen2))
     screen1.pushButton_2.clicked.connect(lambda: changeWindow(screen1, screen2DataCollection))
+
     screen2DataCollection.pushButton_2.clicked.connect(
         lambda: changeDataCollectionScreen(screen2DataCollection, dataCollectionScreen, "Polling"))
     screen2DataCollection.pushButton.clicked.connect(
         lambda: changeDataCollectionScreen(screen2DataCollection, dataCollectionScreen, "Showcase"))
+    screen2DataCollection.pushButton_3.clicked.connect(
+        lambda: changeDataCollectionScreen(screen2DataCollection, dataCollectionScreen, "Text"))
     screen2.pushButton_2.clicked.connect(lambda: changeWindow(screen2, pollingScreen))
     screen2.pushButton.clicked.connect(lambda: changeWindow(screen2, showCaseScreen))
     screen2.commandLinkButton.clicked.connect(lambda: changeWindow(screen2, screen1))
+    screen2.pushButton_3.clicked.connect(lambda: changeWindow(screen2, conversationScreen))
+
     pollingScreen.commandLinkButton.clicked.connect(lambda: changeWindow(pollingScreen, screen2))
+
     showCaseScreen.pushButton_4.clicked.connect(lambda: showWindow(uploadScreen))
 
     uploadScreen.pushButton_3.clicked.connect(lambda: labelText(showCaseScreen, uploadScreen,
@@ -80,7 +95,10 @@ def main():
 
     dataCollectionScreen.commandLinkButton.clicked.connect(lambda: changeWindow(dataCollectionScreen, screen1))
 
+    conversationScreen.commandLinkButton.clicked.connect(lambda: changeWindow(conversationScreen, screen2))
+
     showCaseScreen.commandLinkButton.clicked.connect(lambda: changeWindow(showCaseScreen, screen2))
+
     uploadScreen.commandLinkButton.clicked.connect(lambda: changeWindow(uploadScreen, showCaseScreen))
 
     sys.exit(app.exec_())
