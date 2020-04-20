6e0f8cd677b4e0b21a5540e7bd963a17d8ec7ca2
Ningshan <nouyang@edu.uwaterloo.ca>
2020-04-19 21:08:37 -0400

    move phidgets model to inside

---
+++ Display/DisplayUnity/Assets/Scripts/SelectionMenu/Settings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Screen.SetResolution(1920, 1080, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Screen.fullScreen = !Screen.fullScreen;
+        }
+    }
+}
